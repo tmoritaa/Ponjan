2edd830c68ccf59bfbc3bde0fef3f004c6f64fb7
Tatsuya Morita-Ahad <tatsuya.ahad@gmail.com>
2016-06-14 17:01:31 -0400

    Added hidden tiles and adjusted tile colors.

--- Assets/TileGO.cs
+++ Assets/TileGO.cs
@@ -22,21 +22,28 @@ public class TileGO : MonoBehaviour {
             return;
         }
 
+        // If tile is in deck or is controlled by AI, should be hidden.
+        if (this.tile.Zone.Type == DeckZone.ZoneType.Deck || this.tile.Owner.ControllerType == Player.PlayerType.AI) {
+            this.image.color = new Color(0, 0, 0);
+            this.text.text = "";
+            return;
+        }
+
         switch(this.tile.Type) {
             case Tile.TileType.Red:
-                this.image.color = new Color(1, 0, 0);
+                this.image.color = new Color(1, 0.4f, 0.4f);
                 break;
             case Tile.TileType.Blue:
-                this.image.color = new Color(0, 0, 1);
+                this.image.color = new Color(0, 0.8f, 0.8f);
                 break;
             case Tile.TileType.Yellow:
-                this.image.color = new Color(1, 1, 0);
+                this.image.color = new Color(1, 1, 0.6f);
                 break;
             case Tile.TileType.White:
                 this.image.color = new Color(1, 1, 1);
                 break;
             case Tile.TileType.Dragon:
-                this.image.color = new Color(0, 1, 0);
+                this.image.color = new Color(0.4f, 0.8f, 0);
                 break;
         }
 

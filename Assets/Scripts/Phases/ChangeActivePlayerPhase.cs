e3a418abff81ebbf920282d58597e02943130edf
Tatsuya Morita-Ahad <tatsuya.ahad@gmail.com>
2016-06-16 10:40:59 -0400

    Implemented game completion when no more tiles to draw

--- Assets/Scripts/Phases/ChangeActivePlayerPhase.cs
+++ Assets/Scripts/Phases/ChangeActivePlayerPhase.cs
@@ -8,10 +8,15 @@ public class ChangeActivePlayerPhase : PhaseNode {
     public ChangeActivePlayerPhase() : base(PhaseID.ChangeActivePlayer) {}
 
     public override IEnumerator PerformPhase(Game game) {
-        List<Player> players = game.Players;
-        Player curActivePlayer = players.Find(p => p.IsActive);
-        Player nextActivePlayer = players[(curActivePlayer.Id + 1) % players.Count];
-        game.EnqueueCommand(new ChangeActivePlayerCommand(curActivePlayer, nextActivePlayer));
+        if (game.Deck.Tiles.Count <= 0) {
+            game.GameComplete = true;
+        } else {
+            List<Player> players = game.Players;
+            Player curActivePlayer = players.Find(p => p.IsActive);
+            Player nextActivePlayer = players[(curActivePlayer.Id + 1) % players.Count];
+            game.EnqueueCommand(new ChangeActivePlayerCommand(curActivePlayer, nextActivePlayer));
+        }
+        
         yield break;
     }
 }
